139c9daec74df550bf2e1e1a73799e06030b3d41
snorlax93 <lasanamilton@icloud.com>
2021-11-06 10:46:45 -0400

    removing database loader for now

--- src/main/java/com/snorlaxx/passwordapp/DatabaseLoader.java
+++ src/main/java/com/snorlaxx/passwordapp/DatabaseLoader.java
@@ -1,36 +1,36 @@
-package com.snorlaxx.passwordapp;
+// package com.snorlaxx.passwordapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.stereotype.Component;
 
-@Component
-public class DatabaseLoader implements CommandLineRunner {
+// @Component
+// public class DatabaseLoader implements CommandLineRunner {
 
-    private UserRepository userDB;
-    private AccountRepository actDB;
-    private LogsRepository logDB;
+//     private UserRepository userDB;
+//     private AccountRepository actDB;
+//     private LogsRepository logDB;
 
-    @Autowired
-    public DatabaseLoader(UserRepository userDB, AccountRepository actDB, LogsRepository logDB) {
-        this.userDB = userDB;
-        this.actDB = actDB;
-        this.logDB = logDB;
-    }
+//     @Autowired
+//     public DatabaseLoader(UserRepository userDB, AccountRepository actDB, LogsRepository logDB) {
+//         this.userDB = userDB;
+//         this.actDB = actDB;
+//         this.logDB = logDB;
+//     }
 
-    @Override
-    public void run(String... strings) throws  Exception {
-        this.userDB.save(new User("test1@email.com", "password1", 0));
-        this.userDB.save(new User("test2@email.com", "password1", 1));
-        this.userDB.save(new User("test3@email.com", "password1", 2));
-        this.userDB.save(new User("test3@email.com", "password1", 3));
-        this.actDB.save(new Account("kohls", "kohlspassword", "6146689461", 1L));
-        this.actDB.save(new Account("nike", "nikepassword", "6146689462", 2L));
-        this.actDB.save(new Account("adidas", "adidaspassword", "6146689463", 3L));
-        this.actDB.save(new Account("underarmor", "underarmorpassword", "6146689464", 4L));
-        this.actDB.save(new Account("krogers", "krogerspassword", "6146689465", 5L));
-        this.logDB.save(new Log(1L, "Something is happening"));
-        this.logDB.save(new Log(2L, "That's why we're logging it"));
-        this.logDB.save(new Log(3L, "Aren't you excited"));
-    }
-}
+//     @Override
+//     public void run(String... strings) throws  Exception {
+//         this.userDB.save(new User("test1@email.com", "password1", 0));
+//         this.userDB.save(new User("test2@email.com", "password1", 1));
+//         this.userDB.save(new User("test3@email.com", "password1", 2));
+//         this.userDB.save(new User("test3@email.com", "password1", 3));
+//         this.actDB.save(new Account("kohls", "kohlspassword", "6146689461", 1L));
+//         this.actDB.save(new Account("nike", "nikepassword", "6146689462", 2L));
+//         this.actDB.save(new Account("adidas", "adidaspassword", "6146689463", 3L));
+//         this.actDB.save(new Account("underarmor", "underarmorpassword", "6146689464", 4L));
+//         this.actDB.save(new Account("krogers", "krogerspassword", "6146689465", 5L));
+//         this.logDB.save(new Log(1L, "Something is happening"));
+//         this.logDB.save(new Log(2L, "That's why we're logging it"));
+//         this.logDB.save(new Log(3L, "Aren't you excited"));
+//     }
+// }
