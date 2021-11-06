edced19b3bab6f3c456cafa3fa9c35838ebabb5d
snorlax93 <lasanamilton@icloud.com>
2021-11-06 18:19:34 -0400

    adding the file..

---
+++ src/main/app/src/app.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const React = require('react'); 
+const ReactDOM = require('react-dom');
+const client = require('./client');
+
+class App extends React.Component { 
+
+	constructor(props) {
+		super(props);
+		this.state = {users: []};
+	}
+
+	componentDidMount() { 
+		client({method: 'GET', path: '/api/users'}).done(response => {
+			this.setState({users: response.entity._embedded.users});
+		});
+	}
+
+	render() {
+		return (
+			<EmployeeList users={this.state.users}/>
+		)
+	}
+}
+
+class EmployeeList extends React.Component{
+	render() {
+		const users = this.props.users.map(user =>
+			<User key={user._links.self.href} user={user}/>
+		);
+		return (
+			<table>
+				<tbody>
+					<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Description</th>
+					</tr>
+					{users}
+				</tbody>
+			</table>
+		)
+	}
+}
+
+class User extends React.Component{
+	render() {
+		return (
+			<tr>
+				<td>{this.props.user.username}</td>
+				<td>{this.props.user.password}</td>
+				<td>{this.props.user.usergroup}</td>
+			</tr>
+		)
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('react')
+)
